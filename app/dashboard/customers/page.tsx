4bc0009731b0455f1bb737816287e6df9a9cb92b
IsakGunnardo <127835187+IsakGunnardo@users.noreply.github.com>
2024-04-02 16:06:11 +0200

    init

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Page() {
+    return (
+    <>
+    <p>Customers</p>
+    <p><Link href="/">Go back</Link></p>
+    </>
+        )
+  }
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Page() {
+    return (
+    <>
+    <p>Dashboard</p>
+    <p><Link href="./customers">Customers</Link></p>
+    </>
+        )
+  }
